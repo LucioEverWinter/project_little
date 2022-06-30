c86ded71fd4d83d5114063e609fbaf766e199a57
LucioEverWinter <59634962+LucioEverWinter@users.noreply.github.com>
2022-06-30 11:13:28 +0700

    Add files via upload

--- PROJECT_LITTLE/routes/web.php
+++ PROJECT_LITTLE/routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\FrontController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('home', [FrontController::class, 'home'])->name('home');
