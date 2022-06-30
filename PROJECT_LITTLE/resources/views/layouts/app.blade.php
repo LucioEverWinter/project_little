bad8abe600e3aad3515be0a1d90f922231f81e7d
LucioEverWinter <59634962+LucioEverWinter@users.noreply.github.com>
2022-06-30 11:14:43 +0700

    Add files via upload

--- PROJECT_LITTLE/resources/views/layouts/app.blade.php
+++ PROJECT_LITTLE/resources/views/layouts/app.blade.php
@@ -7,29 +7,35 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link ref="stylesheet" type="text/css" href="{{url('css/main.css')}}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
        
             
-            <nav class="navbar navbar-expand-lg bg-light">
+            <nav class="navbar navbar-expand" style="background-color: #FF7A00;">
                 <div class="logo">
                     <img src="{{ asset('img/Little_ngang.png') }}" alt="logo little ngang">
                 </div>
-                </button>
+                
             
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     @foreach ($navbars as $navbarItem)
-                                <li class="tags">
-                                    <a class="nav-link" href="{{ route($navbarItem->route) }}">{{ $navbarItem->name }}</a>
-                                </li>
+                                <form class="container-fluid justify-content-start">
+                                    <button class="btn btn-outline-light" type="button">
+                                        <a class="nav-link text-light" href="{{ route($navbarItem->route) }}">{{ $navbarItem->name }}</a>
+                                    </button>
+                                </form>
+                                <!--<li class="tags">
+                                    
+                                </li>-->
                             @endforeach
                 </ul>
                 
-                </div>
+            </div>
+
             </nav>
   
         <main class="py-4">
